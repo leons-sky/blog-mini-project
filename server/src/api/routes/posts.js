eadf38aec98df2d8af718a4b53c65eef16ec304e
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2022-11-23 11:18:52 +0000

    commit

--- server/src/api/routes/posts.js
+++ server/src/api/routes/posts.js
@@ -3,4 +3,8 @@ import { Post } from "../../db/models";
 
 const postsRouter = Router();
 
+postsRouter.get("/", async (req, res) => {
+    res.send(await Post.findAll())
+})
+
 export default postsRouter;
